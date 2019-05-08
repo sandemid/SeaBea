ce45c3742e95e9ad66299f5819e2a91286f620a6
Alexandr Demidenkov <san_dem@inbox.ru>
2019-05-08 23:49:27 +0300

    Change Authorize layout 1. Change spinner 2. Create spinner style, add adapter 3. some fixes..

--- app/src/main/java/com/seabea/android/AuthorizeActivity.java
+++ app/src/main/java/com/seabea/android/AuthorizeActivity.java
@@ -3,13 +3,16 @@ package com.seabea.android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.rengwuxian.materialedittext.MaterialEditText;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
@@ -26,9 +29,13 @@ import org.json.JSONException;
 public class AuthorizeActivity extends MvpAppCompatActivity
         implements AuthorizeView, View.OnClickListener {
 
-    TextView txtSignUp;
+    Spinner spSelectAccType;
+    MaterialEditText metLogin;
+    MaterialEditText metPassword;
     Button btnSignIn;
+    TextView txtSignUp;
     ImageButton btnSignVK;
+
 
 
     @InjectPresenter
@@ -46,9 +53,20 @@ public class AuthorizeActivity extends MvpAppCompatActivity
         txtSignUp = findViewById(R.id.t_sign_up);
         btnSignIn = findViewById(R.id.b_sign_in);
         btnSignVK = findViewById(R.id.ib_sign_vk);
+        metLogin = findViewById(R.id.met_login);
+        metPassword = findViewById(R.id.met_password);
+        spSelectAccType = findViewById(R.id.spinner_account_type);
+        setViewMethods();
+    }
+
+    private void setViewMethods() {
         btnSignIn.setOnClickListener(this);
         txtSignUp.setOnClickListener(this);
         btnSignVK.setOnClickListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_select_acc_type_item
+                , getResources().getStringArray(R.array.spinner_select_acc_type));
+        adapter.setDropDownViewResource(R.layout.spinner_select_acc_type_item);
+        spSelectAccType.setAdapter(adapter);
     }
 
     @Override
